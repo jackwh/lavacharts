fa5c89defcac9c09f483b98e1955ffc860e5ff69
Jack Webb-Heller <JackWH@users.noreply.github.com>
2024-04-26 21:16:58 +0100

    Missing properties

--- src/Lavacharts.php
+++ src/Lavacharts.php
@@ -69,6 +69,9 @@ class Lavacharts
      */
     private $scriptManager;
 
+    private ChartFactory $chartFactory;
+    private DashboardFactory $dashFactory;
+
     /**
      * Lavacharts constructor.
      */
